10a0ea02b75ac57dba944026cefc324c98d2adc9
Maria Luiza <mariacalvi900@gmail.com>
2024-09-05 21:45:42 -0300

    tentando consertar e só estragando mais - MCalvi

--- routes/produtos_routes.py
+++ routes/produtos_routes.py
@@ -12,8 +12,24 @@ templates = obter_jinja_templates("templates/produtos")
 
 @router.get("/produtosparacasa", response_class=HTMLResponse)
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/produtosCasaVerTudo.html", {"request": request})
+    return templates.TemplateResponse("pages/produtosCasa.html", {"request": request})
+
+@router.get("/gruposcomunidades", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/gruposComunidades.html", {"request": request})
 
 @router.get("/portachaves", response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("pages/produtoDetalhes_PortaChaves.html", {"request": request})
+
+@router.get("/vasoceramica", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/produtoDetalhes_VasoCeramica.html", {"request": request})
+
+@router.get("/quadrobordado", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/produtoDetalhes_QuadroBordado.html", {"request": request})
+
+@router.get("/bordado", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/produtoDetalhes_Bordado.html", {"request": request})
