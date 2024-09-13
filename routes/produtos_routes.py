cf42e290d1c24c40f42966eaef11ea87c8ec71d9
Maria Luiza <mariacalvi900@gmail.com>
2024-09-13 11:40:39 -0300

    aaaaaa

--- routes/produtos_routes.py
+++ routes/produtos_routes.py
@@ -12,18 +12,6 @@ templates = obter_jinja_templates("templates/produtos")
 
 
 
-@router.get("/portachaves", response_class=HTMLResponse)
+@router.get("/detalhes", response_class=HTMLResponse)
 async def get_root(request: Request):
-    return templates.TemplateResponse("pages/produtoDetalhes_PortaChaves.html", {"request": request})
-
-@router.get("/vasoceramica", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/produtoDetalhes_VasoCeramica.html", {"request": request})
-
-@router.get("/quadrobordado", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/produtoDetalhes_QuadroBordado.html", {"request": request})
-
-@router.get("/bordado", response_class=HTMLResponse)
-async def get_root(request: Request):
-    return templates.TemplateResponse("pages/produtoDetalhes_Bordado.html", {"request": request})
+    return templates.TemplateResponse("pages/produtoDetalhes.html", {"request": request})
