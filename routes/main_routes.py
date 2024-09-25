b0d5b8e5b6acc59a6fd514ff4995bab7b9f91621
mariaclarafg <fricogrillomc@gmail.com>
2024-09-25 11:54:16 -0300

    aaaa

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -9,70 +9,8 @@ from util.templates import obter_jinja_templates
 router = APIRouter()
 templates = obter_jinja_templates("templates/main")
 
-@router.get("/")
-async def get_root(request: Request):
-    usuario = request.state.usuario if hasattr(request.state, "usuario") else None
-    if not usuario:
-        return templates.TemplateResponse("pages/entrar.html", {"request": request})
-    if usuario.perfil == 1:
-        return RedirectResponse("/aluno", status_code=status.HTTP_303_SEE_OTHER)
-    if usuario.perfil == 2:
-        return RedirectResponse("/professor", status_code=status.HTTP_303_SEE_OTHER)
 
-@router.post("/post_entrar")
-async def post_entrar(
-    email: str = Form(...), 
-    senha: str = Form(...)):
-    usuario = UsuarioRepo.checar_credenciais(email, senha)
-    if usuario is None:
-        response = RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
-        return response
-    token = criar_token(usuario[0], usuario[1], usuario[2])
-    nome_perfil = None
-    match (usuario[2]):
-        case 1: nome_perfil = "aluno"
-        case 2: nome_perfil = "professor"
-        case _: nome_perfil = ""
-    
-    response = RedirectResponse(f"/{nome_perfil}", status_code=status.HTTP_303_SEE_OTHER)    
-    response.set_cookie(
-        key=NOME_COOKIE_AUTH,
-        value=token,
-        max_age=3600*24*365*10,
-        httponly=True,
-        samesite="lax"
-    )
-    return response
 
-@router.get("/cadastrar")
-async def get_cadastrar(request: Request):
-    return templates.TemplateResponse("pages/cadastrar.html", {"request": request})
-
-@router.post("/post_cadastrar")
-async def post_cadastrar(
-    nome: str = Form(...),
-    email: str = Form(...),
-    telefone: str = Form(...),
-    senha: str = Form(...),
-    confsenha: str = Form(...),
-    perfil: int = Form(...)):
-    if senha != confsenha:
-        return RedirectResponse("/cadastrar", status_code=status.HTTP_303_SEE_OTHER)
-    senha_hash = obter_hash_senha(senha)
-    usuario = Usuario(None, nome, email, telefone, senha_hash, None, perfil)
-    UsuarioRepo.inserir(usuario)
-    return RedirectResponse("/", status_code=status.HTTP_303_SEE_OTHER)
-
-@router.get("/sair")
-async def get_sair():
-    response = RedirectResponse("/", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
-    response.set_cookie(
-        key=NOME_COOKIE_AUTH,
-        value="",
-        max_age=1,
-        httponly=True,
-        samesite="lax")
-    return response    
 
 
 @router.get("/", response_class=HTMLResponse)
