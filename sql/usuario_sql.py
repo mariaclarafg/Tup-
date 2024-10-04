ff07191b477d155d03c94cb4f1177b9da5c3674d
mariaclarafg <fricogrillomc@gmail.com>
2024-10-04 14:40:46 -0300

    aa

--- sql/usuario_sql.py
+++ sql/usuario_sql.py
@@ -1,12 +1,20 @@
 SQL_CRIAR_TABELA = """
     CREATE TABLE IF NOT EXISTS usuario (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        senha TEXT NOT NULL,
-        perfil INT NOT NULL,        
-        token TEXT)
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nome TEXT NOT NULL, 
+    email TEXT NOT NULL UNIQUE,
+    telefone TEXT NOT NULL UNIQUE, 
+    senha TEXT NOT NULL,
+    tema TEXT NOT NULL,
+    perfil INTEGER NOT NULL)
 """
+
+SQL_INSERIR_USUARIO = """
+    INSERT INTO usuario 
+    (nome, email, telefone, senha, tema, perfil)
+    VALUES (?, ?, ?, ?, "default", ?)
+"""
+
 
 SQL_CHECAR_CREDENCIAIS = """
     SELECT nome, email, perfil, senha
@@ -26,48 +34,13 @@ SQL_ATUALIZAR_SENHA = """
     WHERE email = ?
 """
 
-SQL_EXCLUIR_USUARIO = """
-    DELETE FROM usuario
+SQL_ATUALIZAR_TEMA = """
+    UPDATE usuario
+    SET tema = ?
     WHERE email = ?
 """
 
-SQL_INSERIR_USUARIO = """
-    INSERT INTO usuario(id, nome, email, senha, perfil)
-    VALUES (?, ?, ?)
-"""
-
-
-SQL_ALTERAR_TOKEN = """
-    UPDATE usuario
-    SET token=?
-    WHERE id=?
-"""
-
-SQL_OBTER_POR_ID = """
-    SELECT id, nome, email, perfil, token
-    FROM usuario
-    WHERE id=?
-"""
-
-SQL_OBTER_POR_EMAIL = """
-    SELECT id, nome, email, perfil, token
-    FROM usuario
-    WHERE id=?
-"""
-
-SQL_OBTER_POR_TOKEN = """
-    SELECT id, nome, email, perfil, token
-    FROM usuario
-    WHERE token=?
-"""
-
-SQL_OBTER_QUANTIDADE = """
-    SELECT COUNT(*)
-    FROM usuario
-"""
-
-SQL_EMAIL_EXISTE = """
-    SELECT COUNT(*)
-    FROM usuario
-    WHERE email=?
+SQL_EXCLUIR_USUARIO = """
+    DELETE FROM usuario
+    WHERE email = ?
 """
