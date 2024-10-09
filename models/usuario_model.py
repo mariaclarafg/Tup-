836d763ad563375d8ff0791d2f335e22a990419c
mariaclarafg <fricogrillomc@gmail.com>
2024-10-09 10:42:42 -0300

    aa

--- models/usuario_model.py
+++ models/usuario_model.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
+from datetime import date
 from typing import Optional
 
 
 @dataclass
 class Usuario:
     id: Optional[str] = None
-    nome: Optional[str] = None
+    data_nascimento: Optional[str] = None
+    cpf: Optional[str] = None
     email: Optional[str] = None
-    senha: Optional[str] = None
+    telefone: Optional[str] = None
+    senha: Optional[date] = None
     perfil: Optional[int] = None
-    token: Optional[str] = None
+    
